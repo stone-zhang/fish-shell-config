9dc0eb3ee2ef2912f95d6cdae1a0433b076c0b50
stone <15565292171@163.com>
2025-03-01 15:25:47 +0800

    first

---
+++ config.fish
@@ -0,0 +1,20 @@
+if status is-interactive
+    # 定义自定义提示符
+    function fish_prompt
+        # 获取当前用户名
+        set -l user (whoami)
+        # 获取当前工作目录
+        set -l cwd (prompt_pwd)
+        # 设置提示符颜色
+        set_color normal
+        # 判断是否为超级用户
+        if test $user = "root"
+            # 超级用户提示符
+            echo -n "# $cwd "
+        else
+            # 普通用户提示符
+            echo -n "$cwd \$ "
+        end
+    end
+end
+
